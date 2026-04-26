2449310ea825bb783fc68e9e6f2e30580238a167
Mosetf <stanlym315@gmail.com>
2026-04-26 19:19:40 +0300

    document plan trip request serializer behavior

--- trip_planner/serializers.py
+++ trip_planner/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 
 
 class TripRequestSerializer(serializers.Serializer):
+    """Validate the inputs accepted by the plan-trip endpoint."""
     current_location = serializers.CharField()
     pickup_location = serializers.CharField()
     dropoff_location = serializers.CharField()
@@ -11,6 +12,7 @@ class TripRequestSerializer(serializers.Serializer):
     departure_datetime = serializers.DateTimeField(required=False)
 
     def validate(self, attrs):
+        """Default the departure time to 06:00 when the client omits it."""
         if "departure_datetime" not in attrs:
             attrs["departure_datetime"] = datetime.now().replace(
                 hour=6,
