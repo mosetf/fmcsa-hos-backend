5b6e253a95741aec4dca0456c1aea87e5b2babd6
Mosetf <stanlym315@gmail.com>
2026-04-26 16:37:57 +0300

    settings env validation

--- core/settings.py
+++ core/settings.py
@@ -4,12 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "wkdb?!3!308ngyljgmgt")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("DJANGO_SECRET_KEY is not set")
+
 DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,testserver",
+    "localhost,127.0.0.1"
 ).split(",")
 
 INSTALLED_APPS = [
@@ -80,16 +83,20 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
-USE_TZ = False
+
+USE_TZ = True
+TIME_ZONE = "UTC"
 
 STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = os.getenv(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:3000",
+    "http://localhost:3000"
 ).split(",")
 
-ORS_API_KEY = os.getenv("ORS_API_KEY", "")
+ORS_API_KEY = os.getenv("ORS_API_KEY")
+if not ORS_API_KEY:
+    raise ValueError("ORS_API_KEY is not set")
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
